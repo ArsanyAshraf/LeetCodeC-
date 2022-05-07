4a05951998ba1ae95a4f0f3306a137a7aeeb2774
ArsanyAshraf <89339123+ArsanyAshraf@users.noreply.github.com>
2022-05-07 13:34:57 +0200

    Time: 134 ms (19.77%), Space: 38.4 MB (92.08%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -1,27 +1,32 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-if (s.Length != t.Length) return false;
-            Dictionary<char, int> PairsS = new Dictionary<char, int>();
-            Dictionary<char, int> PairsT = new Dictionary<char, int>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if(!PairsS.TryAdd(s[i], 1))PairsS[s[i]]++;
-                if (!PairsT.TryAdd(t[i], 1)) PairsT[t[i]]++;
-            }
-            for (int i = 0; i < t.Length; i++)
-            {
-                try
-                {
-                    if (PairsS[t[i]] != PairsT[t[i]])
-                    { return false; ; }
-
-                }
-                catch (Exception)
-                {
-
-                    return false;
-                }
-            }
+ if (s == null && t == null)
             return true;
+        
+        if (s == null || t == null)
+            return false;
+        
+        if (s.Length != t.Length)
+            return false;
+        
+        int[] alphabetArray = new int[26];
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            alphabetArray[s[i] - 'a']++;
+        }
+        
+        for (int i = 0; i < t.Length; i++)
+        {
+            alphabetArray[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < alphabetArray.Length; i++)
+        {
+            if (alphabetArray[i] != 0)
+                return false;
+        }
+        
+        return true;
         }
     }
