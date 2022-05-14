d60c0d6626dea3c528af42ee561aadb9463a9094
ArsanyAshraf <89339123+ArsanyAshraf@users.noreply.github.com>
2022-05-14 20:43:46 +0200

    Time: 204 ms (54.88%), Space: 42.5 MB (95.02%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+
+                        for (int j = i + 1; j < nums.Length; j++)
+                        {
+                            if (nums[i] + nums[j] == target)
+                            {
+                                return new int[] { i, j };
+
+                            }
+                        }
+                    }
+
+                
+            
+
+            throw new Exception("no value for two sums");
+            //return new int[]{0} ;
+    }
+}
