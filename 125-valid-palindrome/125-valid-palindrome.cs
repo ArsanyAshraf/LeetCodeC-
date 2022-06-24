d70155259ae003341a5078cde6c48ab25ff0d0c7
ArsanyAshraf <89339123+ArsanyAshraf@users.noreply.github.com>
2022-06-24 12:18:09 +0200

    Time: 162 ms (16.46%), Space: 39.6 MB (48.72%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cs
+++ 125-valid-palindrome/125-valid-palindrome.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool IsPalindrome(string s) {
        var isPalinrome = new string(s.ToLower().ToCharArray().Where(c =>char.IsLetter(c)||char.IsNumber(c) ).ToArray());
 
-            for (int i = 0; i < isPalinrome.Length; i++)
+            for (int i = 0; i < isPalinrome.Length/2; i++)
             {
                 if (isPalinrome[i] != isPalinrome[isPalinrome.Length-1-i])
                 {
